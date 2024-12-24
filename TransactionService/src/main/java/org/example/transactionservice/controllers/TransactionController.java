1a4a67713e8b1978cd6e8da3ef77fdd780c7aafc
Oussama Okbani <oussama.okbani2013@gmail.com>
2024-12-24 16:06:08 +0100

    adding transaction controller

---
+++ TransactionService/src/main/java/org/example/transactionservice/controllers/TransactionController.java
@@ -0,0 +1,64 @@
+package org.example.transactionservice.controllers;
+
+import org.example.transactionservice.dto.TransactionDTO;
+import org.example.transactionservice.entities.Transaction;
+import org.example.transactionservice.enums.Type;
+import org.example.transactionservice.services.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/transactions")
+public class TransactionController {
+
+    @Autowired
+    private TransactionService transactionService;
+
+
+    @GetMapping("/search/all")
+    public List<Transaction> getAllTransactions() {
+        return transactionService.getAllTransactions();
+    }
+
+    @GetMapping("/search/{id}")
+    public Transaction getTransactionById(@PathVariable Long id) {
+        return transactionService.getTransactionById(id);
+    }
+
+    @GetMapping("/search/group/{groupId}")
+    public List<Transaction> getTransactionsByGroupId(@PathVariable Long groupId) {
+        return transactionService.getAllTransactionsByGroupId(groupId);
+    }
+
+    @GetMapping("/search/sender/{senderId}")
+    public List<Transaction> getTransactionsBySenderId(@PathVariable Long senderId) {
+        return transactionService.getAllTransactionsBySenderId(senderId);
+    }
+
+    @GetMapping("/search/reciever/{recieverId}")
+    public List<Transaction> getTransactionsByRecieverId(@PathVariable Long recieverId) {
+        return transactionService.getAllTransactionsByReceiverId(recieverId);
+    }
+
+    @GetMapping("/search")
+    public List<Transaction> getTransactionsByType(@PathVariable String type) {
+        return transactionService.getAllTransactionsByType(type);
+    }
+
+    @PostMapping("/save")
+    public Transaction saveTransaction(@RequestBody TransactionDTO dto) {
+        return transactionService.saveTransaction(dto);
+    }
+
+    @PutMapping("/update/{id}")
+    public Transaction updateTransaction(@PathVariable Long id, @RequestBody TransactionDTO dto) {
+        return transactionService.updateTransaction(id, dto);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteTransaction(@PathVariable Long id) {
+        return transactionService.deleteTransaction(id);
+    }
+}
