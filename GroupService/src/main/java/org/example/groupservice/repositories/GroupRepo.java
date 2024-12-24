09728a09f7e006825b3f5dbff86c81dbed63b5d6
Oussama Okbani <oussama.okbani2013@gmail.com>
2024-12-24 16:00:12 +0100

    adding repositories for group and member

---
+++ GroupService/src/main/java/org/example/groupservice/repositories/GroupRepo.java
@@ -0,0 +1,9 @@
+package org.example.groupservice.repositories;
+
+import org.example.groupservice.entities.Group;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GroupRepo extends JpaRepository<Group, Long> {
+}
---
+++ GroupService/src/main/java/org/example/groupservice/repositories/MemberRepo.java
@@ -0,0 +1,9 @@
+package org.example.groupservice.repositories;
+
+import org.example.groupservice.entities.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepo extends JpaRepository<Member, Long> {
+}
