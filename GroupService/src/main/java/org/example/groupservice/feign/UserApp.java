0ebaad563e86dbb12c48f6eb5ff9a5cd19d5dd59
Oussama Okbani <oussama.okbani2013@gmail.com>
2024-12-24 16:01:54 +0100

    adding user class to wrap the userclient results

---
+++ GroupService/src/main/java/org/example/groupservice/feign/UserApp.java
@@ -0,0 +1,14 @@
+package org.example.groupservice.feign;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @AllArgsConstructor @NoArgsConstructor @Builder
+public class UserApp {
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+}
