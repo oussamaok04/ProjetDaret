2216f2fa2f839fb827a42b69d54fa3f0c2f7a718
Oussama Okbani <oussama.okbani2013@gmail.com>
2025-01-30 00:22:26 +0100

    adding notificaton jpa repository

---
+++ NotificationService/src/main/java/org/example/notificationservice/repositories/NotificationRepository.java
@@ -0,0 +1,9 @@
+package org.example.notificationservice.repositories;
+
+import org.example.notificationservice.entity.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+}
