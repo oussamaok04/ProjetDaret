4676cb22be3fa43fc1337d92601310de00cf081e
Oussama Okbani <oussama.okbani2013@gmail.com>
2025-01-30 00:23:09 +0100

    adding notification entity

---
+++ NotificationService/src/main/java/org/example/notificationservice/entity/Notification.java
@@ -0,0 +1,19 @@
+package org.example.notificationservice.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+public class Notification {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long notificationId;
+    private String message;
+    private boolean isRead;
+}
