0be4d62739290dcc0e35d94019c53cd0454f774c
Oussama Okbani <oussama.okbani2013@gmail.com>
2025-02-01 21:30:41 +0100

    adding createdAt, recievedId and type columns

--- NotificationService/src/main/java/org/example/notificationservice/entity/Notification.java
+++ NotificationService/src/main/java/org/example/notificationservice/entity/Notification.java
@@ -9,11 +9,16 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Data @NoArgsConstructor @AllArgsConstructor @Builder
 public class Notification {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long notificationId;
     private String message;
+    private Long recieverId;
+    private String type;
     private boolean isRead;
+    private LocalDateTime createdAt = LocalDateTime.now();
 }
